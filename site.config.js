0d67ba489456e74f296cdd33769f02b40c9e1363
Kacper Blaz <kacperblaz@gmail.com>
2024-12-21 12:41:51 +0100

    Update projects blog section

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Blog`,
+      href: "https://github.com/KacperBlaz/morethan-log",
     },
   ],
   // blog setting (required)
